3d061fe9e51d7bf4d88161eadb7aeb2253f7ab25
nemeryous <nammp.22it@vku.udn.vn>
2025-09-30 23:39:44 +0700

    minor

---
+++ lib/app/core/theme/app_spacing.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: constant_identifier_names
+
+/// Defines consistent spacing values throughout the application
+class AppSpacing {
+  AppSpacing._();
+  static const SPACE_XS = 4.0;
+
+  static const SPACE_SM = 8.0;
+
+  static const SPACE_MD = 16.0;
+
+  static const SPACE_LG = 24.0;
+
+  static const SPACE_XL = 32.0;
+}
